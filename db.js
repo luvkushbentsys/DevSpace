d512253f6b3029cf95bda83dbe9c33a83983764c
luvkushbentsys <115623334+luvkushbentsys@users.noreply.github.com>
2024-05-04 14:20:41 +0530

    env

--- db.js
+++ db.js
@@ -2,16 +2,16 @@ const MongoClient = require('mongodb').MongoClient;
 const dotenv = require('dotenv')
 dotenv.config()
 
-const url = process.env.URL
+const url = 'mongodb+srv://latest1:test123@cluster0.4qqhdjp.mongodb.net/'
 
-MongoClient.connect(url, (err, client)=>{
-    if(err) throw err
+MongoClient.connect(url, (err, client) => {
+    if (err) throw err
 
     console.log("db connected...")
     module.exports = client
 
     const app = require('./app')
-    app.listen(process.env.PORT, ()=>{
+    app.listen(8000, () => {
         console.log("server listening on 8000...")
     })
 })
